d42f61fee19075b71304ce69d62261e65059e967
Valegoya <150285252+Valegoya@users.noreply.github.com>
2023-12-04 20:07:28 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,50 +1,14 @@
 from flask import Flask,render_template
-import requests
-from dotenv import load_dotenv,dotenv_values
-from sqlalchemy import create_engine, Integer,MetaData,Table, Column, String
 
-metaData=MetaData()
-#definicion de tablas
-cities = Table ('Cities',metaData,
-               Column('id', Integer(),primary_key=True,autoincrement=True),
-               Column('nombre',String(100), nullable=True, unique = True))
-
-
-config = dotenv_values('.env')
 app = Flask(__name__)
-engine = create_engine("sqlite:///weather.db")
 
-def get_weather_data(city):
-    API_KEY = config['API_KEY']
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=es&units=metric'
-    r = requests.get(url).json()
-    print(r)
-    return r 
+@app.route("/home")
+def home():
+    return render_template('pokemon.html')
 
-@app.route('/prueba')
-def prueba():
-    clima=get_weather_data('Ambato')
-    temperatura = str (clima['main']['temp'])
-    descripcion = str(clima['weather'][0]['description'])
-    icono = str(clima['weather'][0]['icon'])
+@app.route("/detalle")
+def detalle():
+    return render_template('detalle.html')
 
-    r_json = {
-              'cuidad' : 'Ambato',
-              'temperatura': temperatura,
-              'descripcion' : descripcion,
-              'icono' : icono
-         }
-    return render_template('weather.html',clima = r_json)
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/clima')
-def clima():
-    return 'Clima'
-
-
-if __name__ == '__main__' :
+if __name__=='__main__':
     app.run(debug=True)
